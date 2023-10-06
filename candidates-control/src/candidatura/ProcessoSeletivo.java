7ba5b2cf83de905f9b395812c3862386e3a62d78
Giov3ss <giovani.sp25@gmail.com>
2023-10-06 23:53:20 +0100

    fazer a impressao dos candidatos selecionados

--- candidates-control/src/candidatura/ProcessoSeletivo.java
+++ candidates-control/src/candidatura/ProcessoSeletivo.java
@@ -1,14 +1,45 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         
     }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de n: " + (indice+1) + " é o " + candidatos[indice]);
+        }
+    }
+
+
+
     static void selecaoCandidatos() {
         // Array com a lista de candidatos
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
-    }
 
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salario " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " Foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    
+    static double valorPretendido() {
+     return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
 
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
@@ -19,8 +50,5 @@ public class ProcessoSeletivo {
         else {
             System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
         }
-            
-        
-        
     }
 }
